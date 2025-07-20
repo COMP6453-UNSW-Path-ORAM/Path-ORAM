fe1e8c486f30b91b0ab574741ceb3580e9f378f7
RYNO8 <35283610+RYNO8@users.noreply.github.com>
2025-07-20 14:16:47 +1000

    test bit_ceil

---
+++ libs/pathoram_client/pathoram_client_tests/test_bit_util.py
@@ -0,0 +1,25 @@
+import unittest
+
+from pathoram_client import bit_util
+
+class TestBitCeil(unittest.TestCase):
+    def test_bit_ceil_0(self):
+        self.assertEqual(bit_util.bit_ceil(0), 1)
+
+    def test_bit_ceil_1(self):
+        self.assertEqual(bit_util.bit_ceil(1), 1)
+    
+    def test_bit_ceil_2(self):
+        self.assertEqual(bit_util.bit_ceil(2), 2)
+    
+    def test_bit_ceil_pow2_minus_one(self):
+        self.assertEqual(bit_util.bit_ceil(127), 128)
+    
+    def test_bit_ceil_pow2(self):
+        self.assertEqual(bit_util.bit_ceil(128), 128)
+    
+    def test_bit_ceil_pow2_plus_one(self):
+        self.assertEqual(bit_util.bit_ceil(129), 256)
+
+if __name__ == "__main__":
+    unittest.main()
